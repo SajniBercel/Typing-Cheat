751a77f2df1f2174706e2de6534472c2ae4cd1ad
SajniBercel <88977595+SajniBercel@users.noreply.github.com>
2023-12-11 00:41:18 +0100

    main

---
+++ Typing_Cheat/Typing_Cheat/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WindowsInput;
+
+namespace Type
+{
+    class prog
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://humanbenchmark.com/tests/typing");
+
+            // Lekérdezzük az összes "myClass" osztállyal rendelkező elemet
+            var elements = driver.FindElements(By.ClassName("incomplete"));
+
+            // Iterálunk az elemeken, és kiírjuk a tartalmukat
+            string szoveg = "";
+            foreach (IWebElement element in elements)
+            {
+                if (element.Text == "" || element.Text == null || element.Text == string.Empty)
+                    szoveg += " ";
+                else
+                    szoveg += element.Text;
+            }
+            InputSimulator input = new InputSimulator();
+
+            Console.WriteLine(szoveg);
+
+            input.Keyboard.TextEntry(szoveg);
+
+            Console.ReadKey();
+
+            driver.Quit();
+        }
+    }
+}
